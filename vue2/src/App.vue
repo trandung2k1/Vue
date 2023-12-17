e48bcdce53950d94cfb41f5cc9826cdeb285c0ea
trandung2k1 <trandungitksnb2001@gmail.com>
2023-12-17 22:39:35 +0700

    create project cli vue2

---
+++ vue2/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'Application',
+  data: function () {
+    return {
+      age: 21,
+      msg: 'Hello',
+    };
+  },
+  components: { Header },
+};
+</script>
+
+<template>
+  <div id="app">
+    {{ age }}
+    <Header v-bind:msg="msg"></Header>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue2/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Header</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['msg'],
+  mounted() {
+    // console.log(this);
+  },
+};
+</script>
+
+<style scoped></style>
