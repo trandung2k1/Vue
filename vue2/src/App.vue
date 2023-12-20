25558f7d1a58c0f1398f954d8abce66327bef4ed
trandung2k1 <trandungitksnb2001@gmail.com>
2023-12-20 23:28:43 +0700

    update vue2

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -3,13 +3,40 @@ import Header from './components/Header.vue';
 
 export default {
   name: 'Application',
-  data: function () {
+  data() {
     return {
       age: 21,
       msg: 'Hello',
+      todos: [],
     };
   },
+  methods: {
+    onSubmit() {
+      console.log(this.msg);
+    },
+  },
+  computed: {},
+  async mounted() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+    const rs = await res.json();
+    this.todos = rs;
+  },
   components: { Header },
+  created() {
+    console.log(this.msg);
+  },
+  watch: {
+    msg(newValue, oldValue) {
+      console.log('new', newValue);
+      console.log('old', oldValue);
+    },
+  },
+  beforeDestroy() {
+    alert('Before Destroyed');
+  },
+  destroyed() {
+    alert('Destroyed');
+  },
 };
 </script>
 
@@ -17,6 +44,10 @@ export default {
   <div id="app">
     {{ age }}
     <Header v-bind:msg="msg"></Header>
+    <form @submit.prevent="onSubmit">
+      <input type="text" v-model="msg" />
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
